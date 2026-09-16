482c66d4fa3ee2874d35c6aaf8da76a3758221fa
soapmactavish23 <ricknogueira1231@gmail.com>
2026-09-15 22:15:59 -0300

    Criando aplicação

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 import fastify from "fastify";
-
 const app = fastify();
-
 app.get("/hello", () => {
-  return "Hello World";
+    return "Hello World";
 });
-
 app
-  .listen({
+    .listen({
     port: 3333,
-  })
-  .then(() => {
+})
+    .then(() => {
     console.log("Server is running on http://localhost:3333");
-  });
+});
