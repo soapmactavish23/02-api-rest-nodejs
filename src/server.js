39233c6c8f3c0bedd02cced2d27872298e7dace9
soapmactavish23 <ricknogueira1231@gmail.com>
2026-09-16 20:36:23 -0300

    Configurando ESLint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-import fastify from "fastify";
-const app = fastify();
-app.get("/hello", () => {
-    return "Hello World";
-});
-app
-    .listen({
-    port: 3333,
+import fastify from 'fastify'
+const app = fastify()
+app.get('/hello', () => {
+  return 'Hello World'
 })
-    .then(() => {
-    console.log("Server is running on http://localhost:3333");
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on http://localhost:3333')
+  })
